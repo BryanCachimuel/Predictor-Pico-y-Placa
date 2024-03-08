edd36795ce397a2caba2053e3fcbba90c89a5166
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-03-08 16:08:53 -0500

    Importando hacia el servidor el archivo donde se encuentran definidas las rutas de los endpints

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { PORT } from './config'
 import morgan from 'morgan'
 import cors from 'cors'
+import predictorRutas from './routes/predictor.routes'
 
 const app = express()
 
@@ -10,6 +11,8 @@ app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+app.use("/predictor",predictorRutas)
 
 
 export default app;
